0606b93872807b2bf486500a3b697d04c7e107f4
bajingxiaozi <getting.xi@gmail.com>
2018-04-15 00:03:30 +0800

    提交代码

--- src/main/java/com/xyf/common/util/FileUtils2.java
+++ src/main/java/com/xyf/common/util/FileUtils2.java
@@ -1,16 +1,16 @@
 package com.xyf.common.util;
 
-import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.io.File;
 
 public class FileUtils2 {
 
-    public static boolean isFile(@Nonnull File file) {
-        return file.exists() && file.isFile();
+    public static boolean isFile(@Nullable File file) {
+        return file != null && file.exists() && file.isFile();
     }
 
-    public static boolean isDirectory(@Nonnull File directory) {
-        return directory.exists() && directory.isDirectory();
+    public static boolean isDirectory(@Nullable File directory) {
+        return directory != null && directory.exists() && directory.isDirectory();
     }
 
 }
