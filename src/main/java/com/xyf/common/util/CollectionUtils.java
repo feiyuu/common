e5b5b42206b9d468959dc571a8ef889a78f6e224
bajingxiaozi <getting.xi@gmail.com>
2018-04-14 17:27:37 +0800

    提交代码

--- src/main/java/com/xyf/common/util/CollectionUtils.java
+++ src/main/java/com/xyf/common/util/CollectionUtils.java
@@ -1,13 +1,18 @@
 package com.xyf.common.util;
 
 import javax.annotation.Nullable;
-import java.util.List;
+import java.util.Collection;
+import java.util.Map;
 
 public class CollectionUtils {
 
-    public static boolean isEmpty(@Nullable List<?> list) {
+    public static boolean isEmpty(@Nullable Collection<?> list) {
         return list == null || list.isEmpty();
-}
+    }
+
+    public static boolean isEmpty(@Nullable Map<?, ?> map) {
+        return map == null || map.isEmpty();
+    }
 
     public static boolean isEmpty(@Nullable Object[] directoryList) {
         return directoryList == null || directoryList.length == 0;
