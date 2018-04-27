87b68575d11959a2b5922f7e4bc55e26cb46558b
bajingxiaozi <getting.xi@gmail.com>
2018-04-28 00:40:57 +0800

    提交代码

--- src/main/java/com/xyf/common/util/FileUtils2.java
+++ src/main/java/com/xyf/common/util/FileUtils2.java
@@ -1,9 +1,12 @@
 package com.xyf.common.util;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.File;
 
 public class FileUtils2 {
+
+    private static final String TAG = FileUtils2.class.getSimpleName();
 
     public static boolean isFile(@Nullable File file) {
         return file != null && file.exists() && file.isFile();
@@ -11,6 +14,20 @@ public class FileUtils2 {
 
     public static boolean isDirectory(@Nullable File directory) {
         return directory != null && directory.exists() && directory.isDirectory();
+    }
+
+    public static void deleteFile(@Nonnull File file) {
+        if (!file.exists()) {
+            Lg.e(TAG, file, "not exist");
+            return;
+        }
+
+        if (file.isFile()) {
+            boolean result = file.delete();
+            Lg.i(TAG, file, result);
+        } else {
+            Lg.e(TAG, "not a file", file);
+        }
     }
 
 }
